2d71d6ea4567735e76bb076f7e6a9455985b9419
Enes Kaya <eneskaya5261@gmail.com>
2022-12-08 20:14:36 +0300

    feat: create 'create a post' function

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import userRoute from "./routes/users.js";
 import authRoute from "./routes/auth.js";
+import postRoute from "./routes/posts.js";
 
 
 dotenv.config();
@@ -21,6 +22,7 @@ app.use(morgan("common"));
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(8800,()=>{
     console.log("Backend server is running!");
